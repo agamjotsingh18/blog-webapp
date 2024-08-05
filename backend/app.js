98ef28a490b3148c523f88a0fc20690b8526f90c
agamjotsingh18 <70067726+agamjotsingh18@users.noreply.github.com>
2024-08-05 15:52:30 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import blogRouter from './routes/blog-routes';
-import router from './routes/user-routes';
+import blogRouter from './routes/blog-routes.js';
+import router from './routes/user-routes.js';
 import cors from 'cors';
-
+NN
 const app = express();
 app.use(cors());
 app.use(express.json());
